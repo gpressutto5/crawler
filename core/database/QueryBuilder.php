6ebc597b5e6c8e2e436af0a9d62d1684bac67067
Guilherme Pressutto <gpressutto5@gmail.com>
2017-02-23 11:03:53 -0300

    Limit query

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -6,10 +6,25 @@ class QueryBuilder
     {
         $this->pdo = $pdo;
     }
-    public function selectAll($table)
+    public function selectAll($table, $limit = null, $offset = 0)
     {
-        $statement = $this->pdo->prepare("select * from {$table}");
+        $query = "SELECT * FROM {$table}";
+        if ($limit) {
+            $query .= " LIMIT :offset,:limit";
+        }
+        $statement = $this->pdo->prepare($query);
+        if ($limit) {
+            $statement->bindParam(":limit", $limit, PDO::PARAM_INT);
+            $statement->bindParam(":offset", $offset, PDO::PARAM_INT);
+        }
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_CLASS);
+    }
+
+    public function countAll($table)
+    {
+        $statement = $this->pdo->prepare("SELECT COUNT(*) AS count FROM {$table}");
+        $statement->execute();
+        return $statement->fetch()['count'];
     }
 }
--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 require 'core/bootstrap.php';
+
+$sites = $app['database']->countAll('woocommerce_list');
+
+var_dump($sites);
+
+return 0;

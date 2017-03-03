0979a41c6e2398c10e91f20caf90ed9aeb5e57c6
Guilherme Pressutto <gpressutto5@gmail.com>
2017-03-03 11:49:48 -0300

    Style

--- helper.php
+++ helper.php
@@ -1,9 +1,10 @@
 <?php
-function is_valid($url) {
-	$headers = get_headers("http://".$url."/cart");
-	$status = substr($headers[0], 9, 3);
-	if ($status === "200") {
-		return true;
-	}
-	return false;
+function is_valid($url)
+{
+    $headers = get_headers("http://".$url."/cart");
+    $status = substr($headers[0], 9, 3);
+    if ($status === "200") {
+        return true;
+    }
+    return false;
 }
--- index.php
+++ index.php
@@ -4,40 +4,40 @@ require 'core/bootstrap.php';
 const NUMBER_OF_INSTANCES_TO_RUN = 10;
 
 if (count($argv) == 1) {
-	$count = count($sites);
-	$limit = $count / NUMBER_OF_INSTANCES_TO_RUN;
+    $count = count($sites);
+    $limit = $count / NUMBER_OF_INSTANCES_TO_RUN;
 
-	for ($i=0; $i < NUMBER_OF_INSTANCES_TO_RUN; $i++) {
-		echo "Iniciando serviço {$i+1} de ".NUMBER_OF_INSTANCES_TO_RUN;
-		$offset = $limit * $i;
-		system("nohup php ~/Code/crawler/index.php $limit $offset > ~/Code/crawler/out{$i+1}.txt");
-		echo "\t\t\tINICIADO\n";
-	}
-	echo NUMBER_OF_INSTANCES_TO_RUN . " serviços iniciados com sucesso!\n";
-	return 0;
+    for ($i=0; $i < NUMBER_OF_INSTANCES_TO_RUN; $i++) {
+        echo "Iniciando serviço {$i+1} de ".NUMBER_OF_INSTANCES_TO_RUN;
+        $offset = $limit * $i;
+        system("nohup php ~/Code/crawler/index.php $limit $offset > ~/Code/crawler/out{$i+1}.txt");
+        echo "\t\t\tINICIADO\n";
+    }
+    echo NUMBER_OF_INSTANCES_TO_RUN . " serviços iniciados com sucesso!\n";
+    return 0;
 }
 
 if (isset($argv[1]) && isset($argv[2])) {
-	$limit = $argv[1];
-	$offset = $argv[2];
-	$sites = $app['database']->select('woocommerce_list', [
-		'id',
-		'url'
-	], $limit, $offset);
+    $limit = $argv[1];
+    $offset = $argv[2];
+    $sites = $app['database']->select('woocommerce_list', [
+        'id',
+        'url'
+    ], $limit, $offset);
 
-	$max = count($sites);
-	$now = 1;
-	foreach ($sites as $value) {
-		echo date("d/m/Y H:i:s");
-		echo " | Checando site ".$now++." de $max | ";
-		echo $value->url;
-		if (is_valid($value->url)) {
-			echo "\tVÁLIDO";
-			$app['database']->selected($value->id);
-		}
-		echo "\n";
-		$app['database']->crawled($value->id);
-	}
+    $max = count($sites);
+    $now = 1;
+    foreach ($sites as $value) {
+        echo date("d/m/Y H:i:s");
+        echo " | Checando site ".$now++." de $max | ";
+        echo $value->url;
+        if (is_valid($value->url)) {
+            echo "\tVÁLIDO";
+            $app['database']->selected($value->id);
+        }
+        echo "\n";
+        $app['database']->crawled($value->id);
+    }
 }
 
 return 0;
